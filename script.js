8af06503ad486b236e223c815672f46fefe7ad06
sha2k24 <sha24.sym@gmail.com>
2024-10-02 13:05:14 +0530

    carosel

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ var navlist = document.querySelector('.navset')
 navmenu.addEventListener('click', function(event){
   navlist.classList.toggle('showmenu')
 })
+
+const carousel = document.querySelector('.carousel')
+const btns = document.querySelectorAll('.btn')
+const imgList = ["carousel_1", "carousel_2","carousel_3"]
+
+btns.forEach((button)=>{
+    button.addEventListener('click', function(event){
+        console.log('clicked')
+    })
+})
