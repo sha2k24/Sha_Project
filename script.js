9dd9268cb5977691c2ca979d0ecf85f2c3d1688b
sha2k24 <sha24.sym@gmail.com>
2024-10-02 11:49:04 +0530

    icon

--- script.js
+++ script.js
@@ -1,39 +1,6 @@
-//Selecting popup box, popup overlay, button
-var popupoverlay = document.querySelector('.popup-overlay')
-var popupbox = document.querySelector('.popup-box')
-var addpopupbutton = document.getElementById('add-popup-button')
+var navmenu = document.querySelector('.menu')
+var navlist = document.querySelector('.navset')
 
-addpopupbutton.addEventListener('click', function(){
-    popupoverlay.style.display = 'block'
-    popupbox.style.display = 'block'
+navmenu.addEventListener('click', function(event){
+  navlist.classList.toggle('showmenu')
 })
-
-
-//Select cancel button
-var cancelbutton = document.getElementById('cancel-popup')
-cancelbutton.addEventListener('click', function(event){
-    event.preventDefault()
-    popupoverlay.style.display = 'none'
-    popupbox.style.display = 'none'
-})
-
-//select container, addbook button,book title, book author, book description
-var container = document.querySelector(".container")
-var addbook = document.getElementById("add-book")
-var booktitleinput = document.getElementById("book-title-input")
-var bookauthorinput = document.getElementById("book-author-input")
-var bookdescriptioninput = document.getElementById("book-description-input")
-
-addbook.addEventListener('click', function(event){
-    event.preventDefault()
-    var div = document.createElement("div")
-    div.setAttribute("class","book-container")
-    div.innerHTML= "<h2>"+booktitleinput.value+"</h2> <h5>"+bookauthorinput.value+"</h5> <p>"+bookdescriptioninput.value+"</p> <button onclick='deletebook(event)'>Delete</button>"
-    container.append(div)
-    popupoverlay.style.display = 'none'
-    popupbox.style.display = 'none'
-})
-
-function deletebook(event) {
-    event.target.parentElement.remove()
-}
