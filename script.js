7bd6d429b0a89aff1e361e2fc2f1d828aaec2091
sha2k24 <sha24.sym@gmail.com>
2024-10-06 13:17:52 +0530

    Version 2.O

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
-var navmenu = document.querySelector('.menu')
-var navlist = document.querySelector('.navset')
+class CarouselImageUpdater {
+  constructor(imageIds, breakpoint) {
+    this.imageIds = imageIds;
+    this.breakpoint = breakpoint;
+    this.defaultImages = {
+      large: [
+        'Sources/Corusal_d1.png',
+        'Sources/Corusal_d2.png',
+        'Sources/Corusal_d3.png',
+        'Sources/Corusal_d4.png'
+      ],
+      small: [
+        'Sources/Corusal_1.jpeg',
+        'Sources/Corusal_2.jpeg',
+        'Sources/Corusal_3.jpeg',
+        'Sources/Corusal_4.jpeg'
+      ]
+    };
 
-navmenu.addEventListener('click', function(event){
-  navlist.classList.toggle('showmenu')
-})
+    // Bind the resize event handler to maintain context
+    this.updateImages = this.updateImages.bind(this);
+    
+    // Initial image update
+    this.updateImages();
+    
+    // Add event listener for window resize
+    window.addEventListener('resize', this.updateImages);
+  }
 
-const carousel = document.querySelector('.carousel')
-const btns = document.querySelectorAll('.btn')
-const imgList = ["carousel_1", "carousel_2","carousel_3"]
+  updateImages() {
+    const screenWidth = window.innerWidth;
+    const imagesToUse = screenWidth > this.breakpoint ? this.defaultImages.large : this.defaultImages.small;
 
-btns.forEach((button)=>{
-    button.addEventListener('click', function(event){
-        console.log('clicked')
-    })
-})
+    this.imageIds.forEach((id, index) => {
+      const imgElement = document.getElementById(id);
+      if (imgElement) {
+        imgElement.src = imagesToUse[index];
+      } else {
+        console.warn(`Image element with ID "${id}" not found.`);
+      }
+    });
+  }
+}
+
+// Usage
+const carouselImageUpdater = new CarouselImageUpdater(['caro1', 'caro2', 'caro3', 'caro4'], 600);
